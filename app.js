e561e3a58dd48ef55b5dbc036d46d0c503ae8f83
ka <ka.kaosf@gmail.com>
2020-03-28 02:16:16 +0900

    Fix to the new ES style

--- app.js
+++ app.js
@@ -3,12 +3,12 @@
  * Module dependencies.
  */
 
-var express = require('express')
+const express = require('express')
   , http = require('http')
   , path = require('path')
   , errorhandler = require('errorhandler');
 
-var app = express();
+const app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -17,14 +17,11 @@ app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
-if ('development' == app.get('env')) {
+if ('development' === app.get('env')) {
   app.use(errorhandler());
 }
 
-app.get('/', function(req, res){
-  res.render('index', { title: 'Express' });
-});
+app.get('/', (req, res) => res.render('index', { title: 'Express' }));
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+http.createServer(app).listen(app.get('port'), () =>
+  console.log('Express server listening on port ' + app.get('port')));
