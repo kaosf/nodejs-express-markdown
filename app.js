f78f5d5158874ca7e15bfc5faf51c3ad255afc1c
ka <ka.kaosf@gmail.com>
2024-05-25 19:07:32 +0900

    Fix by Prettier

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
-
 /**
  * Module dependencies.
  */
 
-const express = require('express')
-  , http = require('http')
-  , path = require('path')
-  , errorhandler = require('errorhandler');
+const express = require("express"),
+  http = require("http"),
+  path = require("path"),
+  errorhandler = require("errorhandler");
 
 const app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'pug');
-app.use(express.static(path.join(__dirname, 'public')));
+app.set("port", process.env.PORT || 3000);
+app.set("views", __dirname + "/views");
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
 
 // development only
-if ('development' === app.get('env')) {
+if ("development" === app.get("env")) {
   app.use(errorhandler());
 }
 
-app.get('/', (req, res) => res.render('index', { title: 'Express' }));
+app.get("/", (req, res) => res.render("index", { title: "Express" }));
 
-http.createServer(app).listen(app.get('port'), () =>
-  console.log('Express server listening on port ' + app.get('port')));
+http
+  .createServer(app)
+  .listen(app.get("port"), () =>
+    console.log("Express server listening on port " + app.get("port"))
+  );
